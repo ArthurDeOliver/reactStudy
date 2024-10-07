48e8d7b79128eb34cb3389ebdd2fbfad6464ebaf
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-10-07 16:23:19 -0300

    aula componentes

---
+++ react-project/src/app/components/Title.tsx
@@ -0,0 +1,9 @@
+export function Title() {
+  return (
+    <>
+      <h1 className="bg-cyan-700 text-2xl font-semibold p-4 text-white text-center">
+        Prática componente
+      </h1>
+    </>
+  );
+}
